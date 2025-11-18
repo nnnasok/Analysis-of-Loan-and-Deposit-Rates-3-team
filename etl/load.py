7e0e64f024fedbc4eb16f20ad7d488073dfc64a9
r9zhenka <r9zhexo@gmail.com>
2025-11-18 20:55:56 +0300

    correct error with add_path

--- etl/load.py
+++ etl/load.py
@@ -26,25 +26,11 @@ def save_to_csv(data, add_dir, filename, self_dir=False):
     
     df = pd.DataFrame(data)
     datestamp = datetime.now(utc_plus_3).strftime('%Y-%m-%d')
+    dir = os.path.join(DATA_DIR, add_dir)
     if self_dir:
-        dir = os.path.join(DATA_DIR, add_dir)
         datestamp = ''
     os.makedirs(dir, exist_ok=True)
     # именование файла: # {SITE}_{TYPE_OFFER}_{DUMP}_{DATE}.csv
     path = os.path.join(dir, f"{filename}_{datestamp}.csv")
     df.to_csv(path, index=False, encoding="utf-8-sig")
     print(f"[OK] Сохранено {len(df)} строк в {filename}")
-
-
-def to_db(df, table_name, db_path="storage/banki_data.db"):
-    """
-    Сохраняет DataFrame в SQL базу данных.
-    Пока не реализована.
-    Заглушка.
-    """
-    return
-    # import sqlite3
-    # conn = sqlite3.connect(db_path)
-    # df.to_sql(table_name, conn, if_exists="replace", index=False)
-    # conn.close()
-    # print(f"[OK] {table_name} записана в {db_path}")
